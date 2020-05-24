e30ce63f8f19a46dae57a26e20e4ad6597473b32
sbaker-dev <samuelbaker.researcher@gmail.com>
2020-05-24 18:49:33 +0100

    reverting Example Use.py

--- Examples/Example Use.py
+++ Examples/Example Use.py
@@ -1,6 +1,39 @@
 from csvObject.csvObject import CsvObject
 
-if __name__ == '__main__':
-    # Simple case, with no data typing
-    csv_object = CsvObject("Example Data.csv")
-    print(csv_object.row_data)
+# Simple case, with no data typing
+print("# Simple Case #")
+csv_object = CsvObject("Example Data.csv")
+print(csv_object.row_data)
+print("")
+
+# Dealing with files without headers
+print("# File Headers #")
+csv_object = CsvObject("Example Data.csv")
+print(csv_object.headers)
+print(csv_object.row_data)
+print("")
+
+csv_object = CsvObject("Example Data.csv", file_headers=False)
+print(csv_object.headers)
+print(csv_object.row_data)
+print("")
+
+# Uniform type
+print("# Uniform Typing #")
+csv_object = CsvObject("Example Data.csv", column_types=int)
+print("")
+print(csv_object.row_data)
+print(csv_object.invalid_typed)
+print("")
+
+# List element typing
+print("# List element Typing #")
+csv_object = CsvObject("Example Data.csv", column_types=[str, float, str, int, bool])
+print(csv_object.row_data)
+
+csv_object = CsvObject("Example Data.csv", column_types=[str, float, str, float, bool], missing_to_zero=True)
+print(csv_object.row_data)
+
+csv_object = CsvObject("Example Data.csv", column_types=[str, float, str, int, bool], print_warnings=False)
+print(csv_object.row_data)
+print("")
